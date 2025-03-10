043feacdccf1b54893d290571ee0379650df62f7
DEVELOPPEUR JAVA <developpeuralvaro@gmail.com>
2025-03-10 12:09:27 +0100

    ecran

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,18 +2,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'gaz-located-frontend',
+  appName: 'FastGaz',
   webDir: 'www',
   server: {
     url: 'https://kapexpert.cloud:30004/',
     cleartext: true
   },
-  android: {
-    fullscreen: false
-  },
-  ios: {
-    fullscreen: false
-  }
+ 
 };
 
 export default config;
