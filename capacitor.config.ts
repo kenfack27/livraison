5d99367203f9b68a3191a985b8a3a62e8128ee71
DEVELOPPEUR JAVA <developpeuralvaro@gmail.com>
2025-03-10 07:43:46 +0100

    Synchronisation Capacitor et mise à jour pour Appflow

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'gaz-located-frontend',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'https://kapexpert.cloud:30004/',
+    cleartext: true
+  }
 };
 
 export default config;
